8275c006b4158f5665e85c5070ac69aeec9f8bdd
Bartlomiej Stachura <b.stachura93@gmail.com>
2015-05-29 21:12:43 +0200

    test zostawienia tylko Bootstrapa

--- src/pl/stachura/projekty/servlet/HelloWorld.java
+++ src/pl/stachura/projekty/servlet/HelloWorld.java
@@ -1,17 +1,17 @@
 package pl.stachura.projekty.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import pl.stachura.projekty.dao.PersonDAO;
-import pl.stachura.projekty.model.Person;
+
+import com.google.gson.Gson;
 
 public class HelloWorld extends HttpServlet {
 
@@ -23,23 +23,30 @@ public class HelloWorld extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-//		resp.setContentType("text/plain"); // sets the content type
-//		
-//		PrintWriter printWriter = resp.getWriter();
-//
-//		PersonDAO personDAO = new PersonDAO();
-//		
-//		//List<Person> persons = personDAO.select();
-//		
-//		printWriter.write("persons");
-////		 resp.setContentType("text/plain");
-//
-		
-		
-		
+		String country = req.getParameter("countryname");
+		Map<String, String> ind = new LinkedHashMap<String, String>();
+		ind.put("1", "New delhi");
+		ind.put("2", "Tamil Nadu");
+		ind.put("3", "Kerala");
+		ind.put("4", "Andhra Pradesh");
+
+		Map<String, String> us = new LinkedHashMap<String, String>();
+		us.put("1", "Washington");
+		us.put("2", "California");
+		us.put("3", "Florida");
+		us.put("4", "New York");
+		String json = null;
+		if (country.equals("India")) {
+			json = new Gson().toJson(ind);
+		} else if (country.equals("US")) {
+			json = new Gson().toJson(us);
+		}
+		resp.setContentType("application/json");
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().write(json);
+
 	}
-	
-	
+
 	public static void main(String[] args) {
 		PersonDAO personDAO = new PersonDAO();
 		personDAO.select();
---
+++ webapp/js/script.js
@@ -0,0 +1,5 @@
+function sayInformationAboutAuthor() {
+	alert("author: Stachura Bartlomiej")
+}
+
+  
